5fa40c9f8c0cba35cd6b5808ab32c516bcc6e234
HjeongMM <159963647+HjeongMM@users.noreply.github.com>
2024-03-26 17:16:38 +0900

    Add files via upload

---
+++ 나이 출력.playground/Contents.swift
@@ -0,0 +1,17 @@
+// 머쓱이는 선생님이 몇 년도에 태어났는지 궁금해졌습니다. 2022년 기준 선생님의 나이 age가 주어질 때, 선생님의 출생 연도를 return 하는 solution 함수를 완성해주세요
+// 제한사항
+// 0 < age ≤ 120
+// 나이는 태어난 연도에 1살이며 매년 1월 1일마다 1살씩 증가합니다.
+
+
+import Foundation
+
+func solution(_ age: Int) -> Int {
+    guard age > 0 && age <= 120 else {
+        fatalError("선생님이 그 나이일리가 없어요..")
+    }
+    return (2022 - age) + 1
+}
+
+// ex)
+print(solution(43))
