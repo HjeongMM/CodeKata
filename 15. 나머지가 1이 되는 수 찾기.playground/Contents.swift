be410901d87b8f9a3f6f0b2a1a272daaa38fbb16
HjeongMM <159963647+HjeongMM@users.noreply.github.com>
2024-04-08 16:09:33 +0900

    Add files via upload

---
+++ 15. 나머지가 1이 되는 수 찾기.playground/Contents.swift
@@ -0,0 +1,17 @@
+//    자연수 n이 매개변수로 주어집니다. n을 x로 나눈 나머지가 1이 되도록 하는 가장 작은 자연수 x를 return 하도록 solution 함수를 완성해주세요. 답이 항상 존재함은 증명될 수 있습니다.
+//
+//    제한사항
+//    3 ≤ n ≤ 1,000,000
+
+import Foundation
+
+func solution(_ n:Int) -> Int {
+    for x in 1... {
+        if n % x == 1 {
+            return x
+        }
+    }
+    return 0
+}
+
+print(solution(12))
