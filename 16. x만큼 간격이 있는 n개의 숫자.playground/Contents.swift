9376ada050a283e4ec46e46d1e46f2bbd14ee417
HjeongMM <159963647+HjeongMM@users.noreply.github.com>
2024-04-09 16:28:19 +0900

    Add files via upload

---
+++ 16. x만큼 간격이 있는 n개의 숫자.playground/Contents.swift
@@ -0,0 +1,20 @@
+//    함수 solution은 정수 x와 자연수 n을 입력 받아, x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴해야 합니다. 다음 제한 조건을 보고, 조건을 만족하는 함수, solution을 완성해주세요.
+//
+//    제한 조건
+//    x는 -10000000 이상, 10000000 이하인 정수입니다.
+//    n은 1000 이하인 자연수입니다.
+
+import Foundation
+
+func solution(_ x:Int, _ n:Int) -> [Int64] {
+    var arr: [Int64] = []
+    var current = Int64(x)
+    
+    while arr.count < n {
+        arr.append(current)
+        current += Int64(x)
+    }
+    return arr
+}
+
+print(solution(-4, 2))
