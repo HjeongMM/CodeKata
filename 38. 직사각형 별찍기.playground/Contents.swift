dd26032993fd5525e175afd193a36a75b8b11836
HjeongMM <159963647+HjeongMM@users.noreply.github.com>
2024-05-01 14:19:28 +0900

    Update Contents.swift

--- 38. 직사각형 별찍기.playground/Contents.swift
+++ 38. 직사각형 별찍기.playground/Contents.swift
@@ -1,16 +1,10 @@
 //이 문제에는 표준 입력으로 두 개의 정수 n과 m이 주어집니다.
 //별(*) 문자를 이용해 가로의 길이가 n, 세로의 길이가 m인 직사각형 형태를 출력해보세요.
 
-//import Foundation
-//
-//let s1 = readLine()!
-//let str = "str"
-
 import Foundation
 
-print("문자열을 입력하세요:")
-if let s1 = readLine() {
-    print("입력받은 문자열: \(s1)")
-} else {
-    print("입력된 문자열이 없습니다.")
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+let (n, m) = (input[0], input[1])
+for _ in 0..<m {
+    print(String(repeating: "*", count: n))
 }
