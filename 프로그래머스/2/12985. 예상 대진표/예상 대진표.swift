3e9fa1636adc0d0aa2bf6d2ec47319c9e3b2116b
HjeongMM <159963647+HjeongMM@users.noreply.github.com>
2024-06-12 13:01:34 +0900

    [level 2] Title: 예상 대진표, Time: 0.06 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+func solution(_ n: Int, _ a: Int, _ b: Int) -> Int {
+    var answer = 0
+    var a = a
+    var b = b
+    
+    while a != b {
+        a = (a + 1) / 2
+        b = (b + 1) / 2
+        answer += 1
+    }
+    print("최종라운드 : \(answer)")
+    return answer
+}
