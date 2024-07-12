420b27a60bac4b58e869cbce9e5e2fae452aef36
HjeongMM <159963647+HjeongMM@users.noreply.github.com>
2024-07-12 12:57:50 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 0.11 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.swift
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.swift
@@ -1,6 +1,13 @@
 import Foundation
 
 func solution(_ num_list:[Int]) -> [Int] {
-    let sortedNum = num_list.sorted(by: <)
-    return Array(sortedNum.dropFirst(5))
+    var sortedNum = num_list.sorted(by: <)
+    var removeCount = 0
+    
+    while removeCount < 5 {
+        sortedNum.removeFirst()
+        removeCount += 1
+    }
+
+    return sortedNum
 }
