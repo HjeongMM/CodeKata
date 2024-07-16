d1a58039ec91cbdff24e0d822321a88c375d35ec
HjeongMM <159963647+HjeongMM@users.noreply.github.com>
2024-07-16 12:47:59 +0900

    [level 0] Title: 배열의 원소만큼 추가하기, Time: 0.32 ms, Memory: 17.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ arr:[Int]) -> [Int] {
+    var result: [Int] = []
+    for i in arr {
+        for _ in 0..<i {
+            result.append(i)
+        }
+    }
+    return result
+}
