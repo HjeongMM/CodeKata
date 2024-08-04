69473ed5612e6c7ab0d6ef0b619f40d701947713
HjeongMM <159963647+HjeongMM@users.noreply.github.com>
2024-08-04 18:14:28 +0900

    [level 0] Title: 카운트 다운, Time: 0.19 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+func solution(_ start_num:Int, _ end_num:Int) -> [Int] {
+    var result: [Int] = []
+    for i in end_num...start_num {
+        result.append(i)
+    }
+    return result.sorted(by: >)
+}
